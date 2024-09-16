d07138168f2ba01cd53134b256ba50cc57275ee0
Enguerrand <enguerrand.ducarme@insa-rennes.fr>
2024-09-16 11:14:51 +0200

    Added one event + remove agenda view

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import BigCalendar from "./components/BigCalendar";
 import "./App.css";
 
 function App() {
-  return <BigCalendar />;
+  return (
+      <div style={{ height: "100vh" }}>
+        <BigCalendar />
+      </div>
+  );
 }
 
 export default App;
--- src/components/BigCalendar.js
+++ src/components/BigCalendar.js
@@ -3,14 +3,20 @@ import moment from "moment";
 
 const localizer = momentLocalizer(moment);
 
+const events = [{
+  start: moment("2024-09-16T10:00:00").toDate(),
+  end: moment("2024-09-16T12:00:00").toDate(),
+  title: "Ui Specification Meeting"
+}]
+
 function BigCalendar() {
   return (
     <Calendar
       localizer={localizer}
-      // events={props.events}
+      events={events}
       startAccessor="start"
       endAccessor="end"
-      style={{ height: 500 }}
+      views={["month", "week", "day"]}
     />
   );
 }
