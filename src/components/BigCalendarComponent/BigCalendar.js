34c4a0a46f537a54006d278eef5299785ca85001
Hao Pan <hapa24@student.bth.se>
2024-09-25 16:46:55 +0200

    toolbar updated

--- src/components/BigCalendarComponent/BigCalendar.js
+++ src/components/BigCalendarComponent/BigCalendar.js
@@ -1,7 +1,71 @@
+import React from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";//Use fachevronleft and fachevronright for the navigation buttons  
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import "./BigCalendar.css"; // import the css file
 
 const localizer = momentLocalizer(moment);
+
+// custom toolbar component 
+const CustomToolbar = (toolbar) => {
+  const goToBack = () => {
+    toolbar.onNavigate('PREV');
+  };
+
+  const goToNext = () => {
+    toolbar.onNavigate('NEXT');
+  };
+
+  const goToCurrent = () => {
+    toolbar.onNavigate('TODAY');
+  };
+
+  const getMiddleButtonText = () => {
+    switch (toolbar.view) {
+      case 'month':
+        return 'This Month';
+      case 'week':
+        return 'This Week';
+      case 'day':
+        return 'Today';
+      default:
+        return 'Today';
+    }
+  };
+
+  const getWeekNumber = () => {
+    if (toolbar.view === 'week') {
+      const currentDate = moment(toolbar.date);
+      return ` (Week ${currentDate.week()} of ${currentDate.year()})`;
+    }
+    return '';
+  };
+
+  return (
+    <div className="rbc-toolbar">
+      <span className="rbc-btn-group">
+        <button type="button" onClick={goToBack}><FaChevronLeft /></button>
+        <button 
+          type="button" 
+          onClick={goToCurrent} 
+          className="middle-button" // add the class name 
+        >
+          {getMiddleButtonText()}
+        </button>
+        <button type="button" onClick={goToNext}><FaChevronRight /></button>
+      </span>
+      <span className="rbc-toolbar-label">
+        {toolbar.label}{getWeekNumber()}
+      </span>
+      <span className="rbc-btn-group">{toolbar.views.map(name => (
+        <button type="button" key={name} className={toolbar.view === name ? 'rbc-active' : ''} onClick={() => toolbar.onView(name)}>
+          {name}
+        </button>
+      ))}</span>
+    </div>
+  );
+};
 
 function BigCalendar({ events, onSelectEvent, onSelectSlot }) {
   return (
@@ -14,6 +78,9 @@ function BigCalendar({ events, onSelectEvent, onSelectSlot }) {
       onSelectEvent={onSelectEvent}
       selectable={true}
       onSelectSlot={onSelectSlot}
+      components={{
+        toolbar: CustomToolbar
+      }}
     />
   );
 }
